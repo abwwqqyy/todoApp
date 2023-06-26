04ce49fb8e2ffd4d6f2e628b5627cc945b2f8d10
abwwqqyy <qiyuewang0315@gmail.com>
2023-06-25 20:37:50 -0500

    added JDBC connector and dependency

---
+++ src/main/java/com/example/todoapp/utils/JDBCUtils.java
@@ -0,0 +1,68 @@
+package com.example.todoapp.utils;
+
+import java.sql.*;
+import java.time.LocalDate;
+
+public class JDBCUtils {
+    private static final String jdbcURL = "jdbc:mysql://localhost:3306/todoapp";
+    private static final String jdbcUsername = "root";
+    private static final String jdbcPassword = "admin";
+
+    public static Connection getConnection(){
+        Connection connection = null;
+        try{
+//            Class.forName("com.mysql.jdbc.driver");
+//            Class.forName("com.mysql.cj.jdbc.driver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void printSQLException(SQLException ex){
+        for (Throwable e : ex){
+            if(e instanceof SQLException){
+                e.printStackTrace(System.err);
+                System.err.println("SQL State: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while(t != null){ // get root cause
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+
+    public static Date getSQLDate(LocalDate date){
+        return Date.valueOf(date);
+    }
+
+    public static LocalDate getUtilDate(Date sqlDate){
+        return sqlDate.toLocalDate();
+    }
+
+    public static void main(String[] args) {
+        try {
+            Statement stmt = getConnection().createStatement();
+            String query = "select * from users";
+            //checks whether query returns the result or not
+            boolean rs = stmt.execute(query);
+            System.out.println("Return value :: " +rs);
+            ResultSet rs1 = stmt.executeQuery(query);
+            while(rs1.next()) {
+                System.out.println("Id :: " +rs1.getInt(1));
+                System.out.println("First Name :: " +rs1.getString("first_name"));
+                System.out.println("Last Name :: " +rs1.getString("last_name"));
+                System.out.println("First Name :: " +rs1.getString(4));
+                System.out.println("First Name :: " +rs1.getString(5));
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+}
